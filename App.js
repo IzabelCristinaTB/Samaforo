6ddb8c0731e0ea3541f1f008d35777bc2d5fcd8e
IZABEL CRISTINA TORRES BATISTA <2019309216@IFAL-MACEIO.INTRANET>
2023-05-18 16:24:20 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const TrafficLight = () => {
+  const [redOn, setRedOn] = useState(true);
+  const [yellowOn, setYellowOn] = useState(false);
+  const [greenOn, setGreenOn] = useState(false);
+
+  const turnOnRed = () => {
+    setRedOn(true);
+    setYellowOn(false);
+    setGreenOn(false);
+  };
+
+  const turnOnYellow = () => {
+    setRedOn(false);
+    setYellowOn(true);
+    setGreenOn(false);
+  };
+
+  const turnOnGreen = () => {
+    setRedOn(false);
+    setYellowOn(false);
+    setGreenOn(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        style={[
+          styles.circle,
+          { backgroundColor: redOn ? 'red' : 'gray' },
+        ]}
+        onPress={turnOnRed}
+      />
+      <TouchableOpacity
+        style={[
+          styles.circle,
+          { backgroundColor: yellowOn ? 'yellow' : 'gray' },
+        ]}
+        onPress={turnOnYellow}
+      />
+      <TouchableOpacity
+        style={[
+          styles.circle,
+          { backgroundColor: greenOn ? 'green' : 'gray' },
+        ]}
+        onPress={turnOnGreen}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000'
+  },
+  circle: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginHorizontal: 20,
+    margin: 10
   },
 });
+
+export default TrafficLight;
